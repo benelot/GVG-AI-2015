5da3ae778c1b0b2860f14aa49ea4eaaec012b737
Benjamin Ellenberger <be.ellenberger@gmail.com>
2015-04-17 11:42:36 +0200

    Add some explanation to the solution.

--- src/tutorials/tutorial1solution/TutorialAgent.java
+++ src/tutorials/tutorial1solution/TutorialAgent.java
@@ -24,7 +24,8 @@ public class TutorialAgent extends AbstractPlayer {
 	 * @param elapsedTimer
 	 *            The time that has elapsed already.
 	 */
-	public TutorialAgent(StateObservation gameState, ElapsedCpuTimer elapsedTimer) {
+	public TutorialAgent(StateObservation gameState,
+			ElapsedCpuTimer elapsedTimer) {
 		randomGenerator = new Random();
 	}
 
@@ -34,7 +35,41 @@ public class TutorialAgent extends AbstractPlayer {
 	 */
 	public ACTIONS act(StateObservation gameState, ElapsedCpuTimer elapsedTimer) {
 		ArrayList<ACTIONS> available_actions = gameState.getAvailableActions();
-		ACTIONS action = available_actions.get(randomGenerator.nextInt(available_actions.size()));
+		ACTIONS action = available_actions.get(randomGenerator
+				.nextInt(available_actions.size()));
+		// ### Another solution would be to get the randomGenerator to produce
+		// ### integers in the range of available actions:
+		//
+		// int actionIndex = randomGenerator.nextInt(available_actions.size());
+		//
+		// ### And then create ifs and else ifs or a switch statement to look
+		// for
+		// ### each number and set the appropriate action to the action
+		// variable.
+		// ### Something like:
+		// if(actionIndex == 0)
+		// {
+		// action = available_actions.get(0);
+		// }
+		// if(actionIndex == 1)
+		// {
+		// action = available_action.get(1);
+		// }
+		// ### and so on until you covered all the actions in the correct
+		// ### QUESTION: Why does it not work to write the number to action
+		// ### mapping explicitly?
+		// ### Answer: The index you generate does not always correspond to
+		// ### the same action, therefore the number to action mapping would be
+		// ### wrong. In the available actions array list are the currently
+		// ### applicable actions listed. It might be that you currently can not
+		// ### go left because something is blocking you out or the game does
+		// ### not offer game items, so you never need ACTIONS.ACTION_USE.
+		// ### Therefore you must choose an action from the available_actions
+		// ### array list.
+		// ### Quick lesson: An ArrayList is a data structure that is like an
+		// ### array (you can use indices to get array items) and like a list
+		// ### (you can use add(item)/remove(item) to add/remove the item
+		// ### to/from the list.
 		return action;
 	}
 }
